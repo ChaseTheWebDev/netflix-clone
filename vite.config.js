cb7cb7f533066cd2d6dd1e9bd4d5e74caf159fdb
Chase <chasetherabbit@me.com>
2024-09-10 15:32:30 -0700

    CSS changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     open: {
-      target: 'http://localhost:3000', // The URL to open
-      // For Google Chrome, specify the browser name or path
-      app: { name: 'chrome' } // Adjust this based on your operating system
+      target: 'http://localhost:3000',
+      app: { name: 'chrome' }
     }
   },
 });
